4307b8a935bea153d9d34a4a98ec8cd4ae02eaca
n00652392 <nejat.ramiz1@huawei.com>
2022-06-21 18:58:31 +0300

    Readme updated and Constant refactored

--- app/src/main/java/com/example/cryptotrackerapp/common/Constants.kt
+++ app/src/main/java/com/example/cryptotrackerapp/common/Constants.kt
@@ -2,26 +2,17 @@ package com.example.cryptotrackerapp.common
 
 object Constants {
     const val BASE_URL = "https://api.coingecko.com/"
-    const val MAX_PRICE = 0
-    const val MIN_PRICE = 0
-    const val RESULT = "RESULT"
-    const val BTC_MAX = "BTC_MAX"
-    const val BTC_MIN = "BTC_MIN"
 
-    // Name of Notification Channel for verbose notifications of background work
-    @JvmField val VERBOSE_NOTIFICATION_CHANNEL_NAME: CharSequence =
+    @JvmField
+    val VERBOSE_NOTIFICATION_CHANNEL_NAME: CharSequence =
         "Verbose WorkManager Notifications"
     const val VERBOSE_NOTIFICATION_CHANNEL_DESCRIPTION =
         "Shows notifications whenever work starts"
-    @JvmField val NOTIFICATION_TITLE: CharSequence = "WorkRequest Starting"
+    @JvmField
+    val NOTIFICATION_TITLE: CharSequence = "Crypto Tracker App"
     const val CHANNEL_ID = "VERBOSE_NOTIFICATION"
     const val NOTIFICATION_ID = 1
 
-    // The name of the image manipulation work
-    const val IMAGE_MANIPULATION_WORK_NAME = "image_manipulation_work"
-
-    // Other keys
-    const val OUTPUT_PATH = "blur_filter_outputs"
     const val KEY_IMAGE_URI = "KEY_IMAGE_URI"
     const val TAG_OUTPUT = "OUTPUT"
 
